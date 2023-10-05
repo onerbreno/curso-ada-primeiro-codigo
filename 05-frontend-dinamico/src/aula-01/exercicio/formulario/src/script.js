1598c5401dcb2392cd5102dcc9092563a9cfe171
Breno <onerbreno@outlook.com>
2023-10-04 23:58:23 -0300

    Frontend dinâmico - Atividade 01: Refatora querySelector dos campos do formulário

--- 05-frontend-dinamico/src/aula-01/exercicio/formulario/src/script.js
+++ 05-frontend-dinamico/src/aula-01/exercicio/formulario/src/script.js
@@ -1,58 +1,58 @@
 const form = document.forms['new-project-form']
 
-const formFields = {
-  projectName: form.elements["project-name"],
-  projectDescription: form.elements["project-description"],
-  projectReferencesURL: form.elements["project-references-url"],
-  projectDueDate: form.elements["project-due-date"],
-  projectColor: form.elements["project-highlight-color"],
-  projectParticipationType: form.elements["project-participation-type"],
-  projectType: form.elements["project-type"],
-}
+const formField = form.querySelectorAll("input, textarea, select")
 
-
-const fields = Object.values(formFields);
-
-fields.forEach((field) => {
-  field.onblur = () => verificaCampo(field);
-});
+formField.forEach(field => field.addEventListener("blur", (event) => {
+  verificaCampo(field)
+}))
 
 function verificaCampo(field) {
-  try {
-    switch (field.type) {
-      case "text":
-        if (field.value === "" || field.value.length > 56) {
-          throw new Error("Nome inválido.")
-        }
-        break;
-
-      case "color":
-        const regexHexColorVerify = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
-      
-        if (regexHexColorVerify.test(field.value)) {
-          throw new Error("Cor inválida.")
-        }
-
-        console.log('color')
-        break;
-
-      case "date":
-        console.log('date')
-        break;
-
-      case "url":
-        console.log('url')
-        break;
-    
-      default:
-        return false
-        break;
-    }
-
-    return true
-  } catch (e) {
-
+  if (field.value === "") {
+    console.error('Campo vazio')
   }
 
-  
+  // try {
+  //   switch (field.type) {
+  //     case "text":
+  //       if (field.value === "") {
+
+  //       }
+
+  //       if (field.value.length > 56) {
+
+  //       };
+  //     case "textarea":
+  //       if (field.value === "") {
+
+  //       }
+
+  //       if (field.value.length > 56) {
+
+  //       };
+
+
+  //     case "color":
+  //       const regexHexColorVerify = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+
+  //       if (regexHexColorVerify.test(field.value)) {
+  //         throw new Error("Cor inválida.")
+  //       }
+  //       break;
+  //     case "date":
+  //       console.log('date')
+  //       break;
+
+  //     case "url":
+  //       console.log('url')
+  //       break;
+
+  //     default:
+  //       return false
+  //       break;
+  //   }
+
+  //   return true
+  // } catch (e) {
+
+  // }
 }
